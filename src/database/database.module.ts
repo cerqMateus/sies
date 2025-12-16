70f1d9eb3ea2c76488e6172c3beaee1db6fe0c09
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-12-16 19:14:17 -0300

    chore: ensure that database is connected correctly

---
+++ src/database/database.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: configService.get<string>('NODE_ENV') !== 'development',
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
